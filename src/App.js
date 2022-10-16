abe9d2ba75b5fa637806af72a42c66071e055c7b
Abul Fajle Azad <abulfajle405@gmail.com>
2022-10-17 02:05:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
+
+  // const students= ["Raaz", "Prasad", "Mayur", "Mansi"];
+  const students = [
+    {name:"Abul", Email:"abc@gmail.com",Id:"253166"},
+    {name:"Rahul", Email:"rahul@gmail.com",Id:"253100"},
+    {name:"Prince", Email:"prince@gmail.com",Id:"253186"},
+    {name:"Guddu", Email:"guddu@gmail.com",Id:"253196"}
+  ]
+  // map looping
+ /* students.map((key)=>{
+    console.warn("my name is :", key)
+  });
+  */
+
+  //in for loop
+ /*
+  for (let i =0; i <students.length; i++){
+    console.log("in for loop", students[i])
+  }
+  */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Handle Array with List</h1>
+      <table border={1} bgcolor="lightblue">
+        <tr bgcolor="skyblue" color="white">
+        <td>Name</td>
+        <td>Email</td>
+        <td>User Id</td>
+        </tr>
+      {
+        students.map((item)=>
+            <tr>
+              <td>{item.name}</td>
+              <td>{item.Email}</td>
+              <td>{item.Id}</td>
+            </tr>
+        )
+      }
+      </table>
     </div>
   );
 }
